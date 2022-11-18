10dc640a45b8197a7a5675390610be3f029f2798
KZLZG <Egor23102002@mail.ru>
2022-11-18 11:23:02 +0300

    reworked models

--- lab3/rowe/models.py
+++ lab3/rowe/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
+# Класс производителя товара
 class Fabricant(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=255, default='Надежный поставщик')
@@ -11,6 +11,7 @@ class Fabricant(models.Model):
         db_table = 'fabricants'
 
 
+# Класс товара
 class Product(models.Model):
     name = models.CharField(max_length=30)
     id_fabricant = models.ForeignKey(Fabricant, on_delete=models.CASCADE)
@@ -23,10 +24,10 @@ class Product(models.Model):
         db_table = 'products'
 
 
+# Класс пользователя сервиса, связанный многие ко многим к товарам с помощью класса помощника Order
 class User(models.Model):
-    login = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    #  orders = models.ManyToManyField(Product)
+    login = models.CharField(max_length=50, verbose_name="Логин")
+    password = models.CharField(max_length=50, verbose_name="Пароль")
     orders = models.ManyToManyField(Product, through='Order')
 
     class Meta:
@@ -34,35 +35,14 @@ class User(models.Model):
         db_table = 'users'
 
 
+# Класс помощник, представляющий таблицу заказов
 class Order(models.Model):
-    users = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ForeignKey(Product, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
+    users = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="id пользователя")
+    products = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="id товара")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Когда создан")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Последнее изменение")
+    status = models.CharField(max_length=16, verbose_name="Статус заказа", default='Создан')
 
-
-'''
-
-class Fabricant(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-
-
-class Product(models.Model):
-    product_name = models.CharField(max_length=50, verbose_name="Название товара")
-    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Цена товара")
-    can_be_delivered = models.BooleanField(verbose_name="Доставляется ли?")
-    in_stock = models.IntegerField(verbose_name="Количество на складе")
-    #id_fabricant = models.ForeignKey(Fabricant, on_delete=models.CASCADE)
-
-
-class User(models.Model):
-    login = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    #favourites = models.ManyToManyField(Product)
-
-
-class Purchase(models.Model):
-    producer = models.ForeignKey(Fabricant, on_delete=models.CASCADE)
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True, verbose_name="Когда совершена покупка");
-'''
+    class Meta:
+        managed: True
+        db_table = 'orders'
