e1a93cd73b86fbeab5056f4bd49f7b943486a72b
KZLZG <Egor23102002@mail.ru>
2023-01-07 22:49:51 +0300

    created redux

---
+++ lab4(Frontend)/my-app/src/redux/cart/cart.reducer.js
@@ -0,0 +1,27 @@
+ import {createSlice} from "@reduxjs/toolkit";
+ import { useSelector } from "react-redux";
+
+
+ const initialState = {
+     cartItems: [],
+ }
+
+ const cartSlice = createSlice({
+     name: "cart",
+     initialState,
+     reducers: {
+         setCartItem: (state, action) =>{
+             state.cartItems.push(action.payload);
+         },
+         deleteCartItem: (state, action) =>{
+             state.cartItems = state.cartItems.filter(item =>item.id !== action.payload)
+         },
+         clearAllItems(state) {
+             state.cartItems = [];
+         },
+     },
+ });
+
+ export const selectAllItems = (state) => state.cartItems;
+ export const { setCartItem, deleteCartItem, clearAllItems} = cartSlice.actions;
+ export default cartSlice.reducer;
---
+++ lab4(Frontend)/my-app/src/redux/store.js
@@ -0,0 +1,8 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import cartReducer from "./cart/cart.reducer";
+
+export default configureStore({
+    reducer: combineReducers({
+        cart: cartReducer
+    })
+});
